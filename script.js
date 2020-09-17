d7848626c9a32cd251f520c6515d8d4e42df5f98
Yi-Jiahe <47210363+Yi-Jiahe@users.noreply.github.com>
2020-09-17 15:14:03 +0800

    Added path

--- script.js
+++ script.js
@@ -9,7 +9,17 @@ const modeshapes =
 	 [0.5933126012761664, 0.45757493372587194, 0.024038371112877977, -0.42122878253207, -0.0008002097209676959],
 	 [0.6395706975325814, 0.7223509367552119, 0.5768617172697054, 0.8985752980439886, 0.0002328297968074114]];
 
+var xScale = 10;
 var yScale = 10;
+
+function calculate_path_d(points){
+	var d = `M ${points[0][0]},${points[0][1]}`;
+	d += ` Q ${(points[1][0]+points[0][0])/2},${(points[0][1]+points[0][1])/2} ${points[1][0]},${points[1][1]}`;	
+	for (var i = 2; i < points.length; i++){
+		d += ` T ${points[i][0]},${points[i][1]}`;
+	}
+	return d;
+}
 
 // Set up SVG display element
 const targetDiv = document.getElementById('svg-target');
@@ -19,20 +29,36 @@ targetDiv.appendChild(svgNode);
 
 // Create SVGs for the masses
 const xPositions = [3.0, 9.0, 15.0, 22.50, 30];
+var initial_displacements = [0, 0, 0, 0, 0];
 const masses = [4000, 6000, 5000, 3000, 1000];
 var svgMasses = [];
 for (var i = 0; i < 5; i++){
 	// create the circle node, set attributes, and append it to the SVG node
 	const circleNode = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-	circleNode.setAttributeNS(null, 'cx', xPositions[i] * yScale);
+	circleNode.setAttributeNS(null, 'cx', xPositions[i] * xScale);
 	circleNode.setAttributeNS(null, 'cy', '50');
 	circleNode.setAttributeNS(null, 'r', Math.pow(masses[i], 1/3));
 	circleNode.setAttributeNS(null, 'fill', 'black');
 	svgNode.appendChild(circleNode);
 	svgMasses.push(circleNode);
 }
+const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+path.setAttributeNS(null, 'd', calculate_path_d(xPositions.map(function(e, i) {return [e*xScale, 50-initial_displacements[i]*yScale]})));
+path.setAttributeNS(null, "stroke", "black"); 
+path.setAttributeNS(null, "stroke-width", 3);  
+path.setAttributeNS(null, "opacity", 1);  
+path.setAttributeNS(null, "fill", "none");
+svgNode.appendChild(path);
 
-let initial_displacements = [0, 0, 0, 0, 0];
+
+function set_mass_displacements(displacements){
+	for (var i = 0; i < 5; i++){
+		svgMasses[i].setAttributeNS(null, 'cy', 50 - displacements[i]*yScale);
+	}
+	const points = xPositions.map(function(e, i) {return [e*xScale, 50-displacements[i]*yScale]});
+	path.setAttributeNS(null, 'd', calculate_path_d(points));
+}
+
 let constants = [0, 0, 0, 0, 0];
 const submitButton = document.getElementById('submit');
 submitButton.disabled = false;
@@ -45,9 +71,7 @@ document.getElementById('initial_displacements').addEventListener('submit', func
 	 document.getElementById('y0_3').value,
 	 document.getElementById('y0_4').value,
 	 document.getElementById('y0_5').value];
-	for (var i = 0; i < 5; i++){
-		svgMasses[i].setAttributeNS(null, 'cy', 50 + initial_displacements[i]*yScale);
-	}
+	set_mass_displacements(initial_displacements);
 	constants = solve_for_constants(initial_displacements);
 });
 
@@ -67,9 +91,7 @@ animation_button.addEventListener('click', function() {
 			animation_button.innerText = 'Reset Animation';
 			break;
 		case 'reset':
-			for (var i = 0; i < 5; i++){
-				svgMasses[i].setAttributeNS(null, 'cy', 50 + initial_displacements[i]*yScale);
-			}
+			set_mass_displacements(initial_displacements);
 			buttonState = 'start';
 			animation_button.innerText = 'Start Animation';
 			break;
@@ -100,10 +122,7 @@ const animate = () => {
 		const t = (timestamp - startTime)/(1000/timeScale);
 		console.log(t);
 		const yPositions = y(t);
-		// console.log(yPositions);
-		for (var i = 0; i < 5; i++){
-			svgMasses[i].setAttributeNS(null, 'cy', 50 + yPositions[i]*yScale);
-		}
+		set_mass_displacements(yPositions);
 		if (play_animation) {
 			window.requestAnimationFrame(animateStep);
 		}
